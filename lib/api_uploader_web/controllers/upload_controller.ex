f83d5647f0788b5d84ccdd2b6353a8def9e92deb
ivorpad <ivorjpc@gmail.com>
2023-07-26 22:23:24 +0200

    remove bad endpoint :D

--- lib/api_uploader_web/controllers/upload_controller.ex
+++ lib/api_uploader_web/controllers/upload_controller.ex
@@ -1,13 +1,6 @@
 defmodule ApiUploaderWeb.UploadController do
   use ApiUploaderWeb, :controller
 
-  def show(conn, _params) do
-    json(conn, %{
-      AWS_ACCESS_KEY_ID: System.get_env("AWS_ACCESS_KEY_ID"),
-      AWS_SECRET_ACCESS_KEY: System.get_env("AWS_SECRET_ACCESS_KEY")
-    })
-  end
-
   def create(conn, params) do
     IO.inspect(params)
 
