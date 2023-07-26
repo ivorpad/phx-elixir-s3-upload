eef60bf0d3f0213db57ed42c953df5961b8c362f
ivorpad <ivorjpc@gmail.com>
2023-07-26 22:14:28 +0200

    maybe this time?

--- config/runtime.exs
+++ config/runtime.exs
@@ -49,6 +49,18 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
+  config :ex_aws,
+    access_key_id: System.get_env("AWS_ACCESS_KEY_ID"),
+    secret_access_key: System.get_env("AWS_SECRET_ACCESS_KEY"),
+    region: System.get_env("AWS_REGION") || "us-east-2"
+  # debug_requests: true
+
+  config :ex_aws, :s3,
+    scheme: "https://",
+    host: "s3-us-east-2.amazonaws.com",
+    bucket: "themeforest-vault",
+    region: "us-east-2"
+
   # ## SSL Support
   #
   # To get SSL working, you will need to add the `https` key
