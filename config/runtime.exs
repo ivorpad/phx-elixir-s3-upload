0d73ae741852cf2777c3bb5f60784bcc01586ee5
ivorpad <ivorjpc@gmail.com>
2023-07-26 21:48:51 +0200

    fixed crash

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,6 +20,8 @@ if System.get_env("PHX_SERVER") do
   config :api_uploader, ApiUploaderWeb.Endpoint, server: true
 end
 
+if config_env() == :prod do
+
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
